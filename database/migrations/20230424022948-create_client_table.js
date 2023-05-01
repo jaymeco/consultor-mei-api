de43aaf25fd2e9042e7fa7c25347a704a20d9257
Jaymeco <jayme.anunciacao@gmail.com>
2023-05-01 14:36:50 +0000

    feature(database/migrations/create_client_table): Cria migração para a criação da tabela de cliente.

---
+++ database/migrations/20230424022948-create_client_table.js
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('client', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        unique: true,
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      company_name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      company_cnpj: {
+        unique: true,
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      state: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+      license_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'license',
+          key: 'id',
+        },
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('client');
+  },
+};
