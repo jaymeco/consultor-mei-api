895581cbde74fbf200c94dc63ab0ac26da016db4
Jaymeco <jayme.anunciacao@gmail.com>
2023-05-13 21:17:24 +0000

    feature(database/migrations): Cria migração para a tabela de status.

---
+++ database/migrations/20230513205808-create-status-table.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('status', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      description: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    });
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('status');
+  },
+};
