a2c9c20fbe1a2a9ca535463f88c366dc0c0f851e
Jaymeco <jayme.anunciacao@gmail.com>
2023-05-13 16:07:55 +0000

    feature(database/migrations): Cria migração para a criação da tabela de segmentos de consultoria.

---
+++ database/migrations/20230513154013-create-consulting-segment-table.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('consulting_segment', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      description: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('consulting_segment');
+  },
+};
