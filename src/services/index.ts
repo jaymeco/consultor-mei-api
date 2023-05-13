ea01a89da9e222513c3dc9d5f64e67edc12f895f
Jaymeco <jayme.anunciacao@gmail.com>
2023-05-13 15:19:36 +0000

    feature(services/LicenseService): Cria service para o recurso de licensa.
    - Adiciona método create.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,20 @@
 import { AuthService } from './AuthService';
 import { AdministratorService } from './AdministratorService';
+import { CnaeService } from './CnaeService';
+import { ClientService } from './ClientService';
+import { LicenseService } from './LicenseService';
 
 const authService = new AuthService();
 const administratorService = new AdministratorService();
+const cnaeService = new CnaeService();
+const clientService = new ClientService();
+const licenseService = new LicenseService();
+
 
 export {
   authService,
+  cnaeService,
+  clientService,
+  licenseService,
   administratorService,
 };
